247604a0cf73461c6674d33a9d2e4f3ae734e523
praneeth999 <chandrapraneeth99@gmail.com>
2025-09-23 12:45:29 -0500

    Add MCPToolFormatter to init file

--- massgen/formatter/__init__.py
+++ massgen/formatter/__init__.py
@@ -4,6 +4,7 @@ Message formatting utilities.
 Provides utility classes for message formatting and conversion.
 """
 from .message_formatter import MessageFormatter
+from .mcp_tool_formatter import MCPToolFormatter
 from .tool_formatter import ToolFormatter
 
-__all__ = ["MessageFormatter", "ToolFormatter"]
+__all__ = ["MessageFormatter", "MCPToolFormatter", "ToolFormatter"]
