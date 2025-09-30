4b2322e86f5c0d3b495ba3c9514c1dc74ab37780
qidanrui <qidanrui@gmail.com>
2025-09-29 18:44:10 -0700

    init tool register support

---
+++ massgen/tools/base_tool.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Any, List
+
+class BaseTool(ABC):
+    """Base class for all tools"""
+    
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        """Tool name for OpenRouter function calling"""
+        pass
+    
+    @property
+    @abstractmethod
+    def description(self) -> str:
+        """Tool description for OpenRouter"""
+        pass
+    
+    @property
+    @abstractmethod
+    def parameters(self) -> Dict[str, Any]:
+        """OpenRouter function parameters schema"""
+        pass
+    
+    @abstractmethod
+    def execute(self, **kwargs) -> Any:
+        """Execute the tool with given parameters"""
+        pass
+    
